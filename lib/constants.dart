7cbb02cae8888c2bf56042bb3b67de7d852d6167
Chamika Dilshan <chamika.dilshan@app360.lk>
2023-07-25 17:46:18 +0530

    added user profile picture saving to firebase

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
+
+var firebaseAuth = FirebaseAuth.instance;
+var firebaseStorage = FirebaseStorage.instance;
+var firestore = FirebaseFirestore.instance;
--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,6 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:flutter/material.dart';
+
 
 class AuthController extends GetxController{
 
+  //upload userImage
+  Future<String> _uploadToStorage(File image) async{
+    Reference ref = firebaseStorage.ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl; //return upload image url
+  }
+  //register the user
+  void registerUser(
+      String userName,
+      String email,
+      String password,
+      File? image
+      ) async{
+    try{
+      if(userName.isNotEmpty
+          && password.isNotEmpty
+          && email.isNotEmpty
+          && image != null){
+
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+            email: email,
+            password: password
+        );
+
+        String downloadUrl = await _uploadToStorage(image);
+      }
+    } catch(e){
+      Get.snackbar('Error Creating Account', e.toString());
+    }
+  }
 }
